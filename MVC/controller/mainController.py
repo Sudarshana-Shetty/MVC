cdda298204360c1115603df81ddbca517ed11059
Sudarshana-Shetty <48707855+Sudarshana-Shetty@users.noreply.github.com>
2019-05-28 12:04:56 +0530

    Update mainController.py

--- MVC/controller/mainController.py
+++ MVC/controller/mainController.py
@@ -4,7 +4,7 @@ from model import dbConn
 app = Flask(__name__)
 
 class MainController(object):
-
+# >>>>>> --- Insert user details to DB --- <<<<<<
     def insertUser(self, uDetails):
         if dbConn.db.UserCollections.count({"Name": uDetails['Name']}) == 0:            
             result = dbConn.db.UserCollections.insert_one(uDetails)
@@ -16,7 +16,8 @@ class MainController(object):
                 return user
         else:
             return 'Duplicate user'
-
+        
+# >>>>>> --- Update the user Address --- <<<<<<
     def updateUser(self, uInput):
         userExist = self.findUser(uInput)
         if userExist:
@@ -50,7 +51,7 @@ class MainController(object):
         else: 
             return 'Please enter valid user details to delete'
 
-
+# >>>>>> --- Display one user details --- <<<<<<
     def displaySingleUser(self, findUser): 
         details = []      
         uCollections = dbConn.db.UserCollections.find({'Name': findUser['Name']}, {'_id':0, 'Name':1, 'Address':1})
@@ -58,7 +59,8 @@ class MainController(object):
             details.append(userColumn)
         # print(details)
         return details
-
+    
+# >>>>>> --- Display all user details --- <<<<<<
     def displayAllUser(self):      
         users = []  
         uCollections = dbConn.db.UserCollections.find({}, {'_id':0, 'Name':1, 'Address':1})
@@ -66,7 +68,8 @@ class MainController(object):
             users.append(allrecord)            
         # print(users)
         return users
-
+    
+# >>>>>> --- Checking for user if exists --- <<<<<<    
     def findUser(self, search):
         if dbConn.db.UserCollections.count({"Name": search['Name']}) == 0:
             return False
